7551cae083c79453c2655f1096827caff55fa8f2
chefTee <taiwosharon36@gmail.com>
2025-06-20 11:12:59 +0100

    Initial commit with 0x02-tailwind-css project

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./1-index.html",
+    "./src/**/*.{html,js}",
+    "./dist/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
